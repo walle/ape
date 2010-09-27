89612a6767332bcb054d3d5b242c9598e7488ac8
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-09-27 23:08:53 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ape::Application.config.secret_token = '7302adc0bdb65fd02074470b36a5da5a9f595664c793c11be741069f10c413bd9a1927a200e65ad87bdc33a9adbbef6308a3d31cd0e0cee8a4497d26d60088bd'
