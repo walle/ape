07485d15aa01542147493d7cae7acd1292351237
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-09-29 23:03:00 +0200

    Add edit page

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -11,15 +11,25 @@ class WikiController < ApplicationController
 
   def create
     page = params[:name].parameterize
-    filename = File.join @project.directory, 'wiki/', page_name(page)
-    File.open filename, 'w' do |f|
-        f.puts params[:content]
-      end
-    git_repository = Git.open @project.directory
-    git_repository.add filename
+    set_content page, params[:content]
     git_repository.commit 'Add page: ' + page
 
     redirect_to project_wiki_url :id => page
+  end
+
+  def edit
+    load_contents @project, params[:id]
+  end
+
+  def update
+    set_content params[:id], params[:content]
+    git_repository.commit 'Update page: ' + page
+
+    redirect_to project_wiki_url :id => params[:id]
+  end
+
+  def destroy
+
   end
 
   private
@@ -28,13 +38,17 @@ class WikiController < ApplicationController
     end
 
     def load_project_data(project, page)
-      filename = File.join project.directory, 'wiki/', page_name(page)
-      @contents = file_contents filename
+      load_contents(project, page)
       if !@contents.empty?
         @contents = RedCloth.new(@contents).to_html.html_safe
       else
         render :status => 404
       end
+    end
+
+    def load_contents(project, page)
+      filename = File.join project.directory, 'wiki/', page_name(page)
+      @contents = file_contents filename
     end
 
     def file_contents(filename)
@@ -48,6 +62,15 @@ class WikiController < ApplicationController
 
     def page_name(page)
       page + '.txt'
+    end
+
+    def set_content(page, content)
+      filename = File.join @project.directory, 'wiki/', page_name(page)
+      File.open filename, 'w' do |f|
+          f.puts content
+        end
+      git_repository = Git.open @project.directory
+      git_repository.add filename
     end
 end
 
