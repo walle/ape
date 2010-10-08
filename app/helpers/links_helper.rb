466d40f7201fb5509ef11eb5e5d1e0aae6b4467b
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-10-08 22:19:37 +0200

    Change link helper to include wiki comments

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,11 +1,10 @@
 module LinksHelper
 
-  def rewrite_wiki_links(wiki)
-    wiki.to_html.gsub(/\[\[(.+)\]\]/) do
+  def rewrite_wiki_links(item)
+    item.to_html.gsub(/\[\[(.+)\]\]/) do
       url = $1.split('/').map { |file| file.parameterize }
-      url = File.join wiki.page, url unless wiki.index? || $1.start_with?('/')
 
-      if (File.exists?(File.join wiki.project.directory, 'wiki', url, 'index.txt'))
+      if (File.exists?(File.join item.project.directory, 'wiki', url, 'index.txt'))
         '<a href="' + (url.empty? ? wiki_index_url : wiki_page_url(:id => url)) + '">' + $1 + '</a>'
       else
         '<a class="new" href="' + wiki_page_url(:id => url) + '">[[' + $1 + ']]</a>'
